fd38a48b140064b7061d9e7dc9173e1b1c50bb62
Christoffer Jansson <ctjansson@gmail.com>
2015-11-27 08:59:58 +0100

    First Commit

---
+++ ConsoleApplication1/ConsoleApplication1/Numbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public static class Numbers
+    {
+        public static int SumOfTwoHighestNumbers(List<int> numbers)
+        {
+            if (numbers == null)
+                return 0;
+
+            int num1 = 0;
+            int num2 = 0;
+            int highestIndex = 0;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] > num1)
+                {
+                    num1 = numbers[i];
+                    highestIndex = i;
+                }
+            }
+
+            for (int j = 0; j < numbers.Count; j++)
+            {
+                if (numbers[j] > num2 && j != highestIndex)
+                    num2 = numbers[j];
+            }
+
+            return num1 + num2;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            List<int> numbers = new List<int>() { 20, 2, 45, 33, 87, 89, 26, 88 };
+
+            int expectedResult = 89 + 88;
+
+            int sumOfTwoHighest = Numbers.SumOfTwoHighestNumbers(numbers);
+
+            Assert.AreEqual(expectedResult, sumOfTwoHighest);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            List<int> numbers = new List<int>() { 15, 15, 10, 18, 22, 19, 21, 22 };
+
+            int expectedResult = 22 + 22;
+
+            int sumOfTwoHighest = Numbers.SumOfTwoHighestNumbers(numbers);
+
+            Assert.AreEqual(expectedResult, sumOfTwoHighest);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            List<int> numbers = new List<int>() { 0, 20, 15, 10, 14, 19, 18, 4, 21 };
+
+            int expectedResult = 20 + 21;
+
+            int sumOfTwoHighest = Numbers.SumOfTwoHighestNumbers(numbers);
+
+            Assert.AreEqual(expectedResult, sumOfTwoHighest);
+        }
+    }
+}
